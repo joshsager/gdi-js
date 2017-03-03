05d863dca2bf6985e5768fef395f6963f349d1ee
joshsager <sager379@gmail.com>
2017-03-02 19:53:10 -0500

    Adds breakpoint example

---
+++ breakpoint.js
@@ -0,0 +1,33 @@
+
+console.log("I am the first thing to execute");
+
+function numberone(){
+	console.log("I am function number one");
+}
+
+function numbertwo(){
+	console.log("I am function number two");
+}
+
+function numberthree(){
+	console.log("I am function number three");
+}
+
+function numberfour(){
+	var beatle = "john";
+
+	if(beatle == "john"){
+		debug;
+	}else if(beatle == "george"){
+		debug;	
+	}else if(beatle == "ringo"){
+		debug;
+	}else if(beatle == "paul"){
+		debug;
+	}
+}
+
+
+numberone();
+numberthree();
+numbertwo();
