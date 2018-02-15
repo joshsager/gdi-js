213ffc8793ecfc5291b492bef701c0c2bd0c0bf7
joshsager <sager379@gmail.com>
2018-02-14 21:09:09 -0500

    adds in debugger for breakpoint example

--- breakpoint.js
+++ breakpoint.js
@@ -1,23 +1,27 @@
+debugger;
 
 console.log("I am the first thing to execute");
 
 function numberone(){
 	console.log("I am function number one");
+	debugger;
 }
 
 function numbertwo(){
 	console.log("I am function number two");
+	debugger;
 }
 
 function numberthree(){
 	console.log("I am function number three");
+	debugger;
 }
 
 function numberfour(beatle){
 	if(beatle == "john"){
 		debugger;
 	}else if(beatle == "george"){
-		debugger;	
+		debugger;
 	}else if(beatle == "ringo"){
 		debugger;
 	}else if(beatle == "paul"){
